f565953b4c213cd6926e5ff0adeccf3e5e52f92e
Saurabh Jain <sj652744@broadcom.net>
2019-08-22 21:12:55 +0530

    copyright logo added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = props => (
   <div>
     {props.createdBy && <p>Created by {props.createdBy}</p>}
-    <h4>Copyright 2019-20</h4>
+    <h4> &#0169; 2019-20</h4>
   </div>
 );
 export default Footer;
